cf74c0469ec8ae5e8c8ec1d47772921822b3006d
omerc4c <omeredizaydin@gmail.com>
2025-05-10 22:25:45 +0300

    book endpoints structure added

--- librarymanagement/src/main/java/com/getir/lms/librarymanagement/controller/BookController.java
+++ librarymanagement/src/main/java/com/getir/lms/librarymanagement/controller/BookController.java
@@ -1,10 +1,51 @@
 package com.getir.lms.librarymanagement.controller;
 
+import com.getir.lms.librarymanagement.dto.BookDto;
+import com.getir.lms.librarymanagement.model.entity.Book;
+import com.getir.lms.librarymanagement.service.book.BookService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
 public class BookController {
+  private final BookService bookService;
+
+  @GetMapping("")
+  public ResponseEntity<List<Book>> getAllBooks() {
+    return ResponseEntity.ok(bookService.getAllBooks());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Book> getBookById(@PathVariable("id") UUID id) {
+    return ResponseEntity.ok(bookService.getBookById(id));
+  }
+
+  @PostMapping("")
+  public ResponseEntity<Book> create(@RequestBody BookDto dto) {
+    return ResponseEntity.ok(bookService.create(dto));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Book> update(@PathVariable("id") UUID id, @RequestBody BookDto dto) {
+    return ResponseEntity.ok(bookService.update(id, dto));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Book> update(@PathVariable("id") UUID id) {
+    bookService.delete(id);
+    return ResponseEntity.noContent().build();
+  }
 
 }
--- librarymanagement/src/main/java/com/getir/lms/librarymanagement/service/book/impl/BookServiceImpl.java
+++ librarymanagement/src/main/java/com/getir/lms/librarymanagement/service/book/impl/BookServiceImpl.java
@@ -1,7 +1,36 @@
 package com.getir.lms.librarymanagement.service.book.impl;
 
+import com.getir.lms.librarymanagement.dto.BookDto;
+import com.getir.lms.librarymanagement.model.entity.Book;
+import com.getir.lms.librarymanagement.service.book.BookService;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookServiceImpl {
+public class BookServiceImpl implements BookService {
+  @Override
+  public List<Book> getAllBooks() {
+    return List.of();
+  }
+
+  @Override
+  public Book getBookById(UUID id) {
+    return null;
+  }
+
+  @Override
+  public Book create(BookDto dto) {
+    return null;
+  }
+
+  @Override
+  public Book update(UUID id, BookDto dto) {
+    return null;
+  }
+
+  @Override
+  public void delete(UUID id) {
+
+  }
 }
